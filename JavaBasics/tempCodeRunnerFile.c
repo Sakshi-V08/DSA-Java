94fcb4a619f04f5b998bf95f8906051585fd8f66
Sakshi-V08 <122119205+Sakshi-V08@users.noreply.github.com>
2023-10-06 17:20:54 +0530

    upload

--- JavaBasics/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int b,a=4;
-    b=- - a;
-    printf("%d",a);
-}
--- JavaFunctions/SqaureRoot.java
+++ JavaFunctions/SqaureRoot.java
@@ -1,4 +1,4 @@
-public class extra {
+public class SqaureRoot {
     public static void main(String[] args) {
         int n = 25; // Your input number
         int result = sqrt(n);
