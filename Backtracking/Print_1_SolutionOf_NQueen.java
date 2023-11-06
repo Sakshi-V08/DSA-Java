145d3301e9f3d4c12aefb9420233597f8dc077a4
Sakshi-V08 <122119205+Sakshi-V08@users.noreply.github.com>
2023-11-06 10:02:38 +0530

    upload

--- Backtracking/Print_1_SolutionOf_NQueen.java
+++ Backtracking/Print_1_SolutionOf_NQueen.java
@@ -1,6 +1,6 @@
 package Backtracking;
 
-public class N_Queen {
+public class Print_1_SolutionOf_NQueen {
     public static boolean isSafe(char board[][], int row, int col) {
         // vertical up
         for (int i = row - 1; i >= 0; i--) {
@@ -25,23 +25,25 @@ public class N_Queen {
         return true;
     }
 
-    public static void nQueens(char board[][], int row) {
+    public static boolean nQueens(char board[][], int row) {
         // base case
         if (row == board.length) {
-            printBoard(board);
+            // printBoard(board);
             count++;
-            return;
+            return true;
         }
 
         // column loop
         for (int j = 0; j < board.length; j++) {
             if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
-                nQueens(board, row + 1); // function call
+                if (nQueens(board, row + 1)) {
+                    return true;
+                } // function call
                 board[row][j] = 'x'; // backtracking
             }
         }
-
+        return false;
     }
 
     public static void printBoard(char board[][]) {
@@ -54,9 +56,11 @@ public class N_Queen {
         }
 
     }
-    static int count =0;
+
+    static int count = 0;
+
     public static void main(String args[]) {
-        int n = 4;
+        int n = 2;
         char board[][] = new char[n][n];
         // intialize
         for (int i = 0; i < n; i++) {
@@ -64,7 +68,12 @@ public class N_Queen {
                 board[i][j] = 'x';
             }
         }
-        nQueens(board, 0);
-        System.out.println("Total ways to solve n queens = "+count);
+        if (nQueens(board, 0)) {
+            System.out.println("Solution is possible");
+            printBoard(board);
+        } else {
+            System.out.println("solution is not possible");
+        }
+
     }
 }
