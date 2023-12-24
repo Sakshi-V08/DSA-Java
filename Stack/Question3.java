d347f8c1a5b6cef05664aeb19d394bbc708e8bf2
Sakshi-V08 <122119205+Sakshi-V08@users.noreply.github.com>
2023-12-24 18:30:21 +0530

    upload

--- Stack/Question3.java
+++ Stack/Question3.java
@@ -1,5 +1,49 @@
 package Stack;
+
 import java.util.*;
+
 public class Question3 {
-    
+    // Push at the Bottom of the stack
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    // Reverse a stack
+
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s) {
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+
+    public static void main(String args[]) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println("Reverse the stack:");
+        //3,2,1
+        reverseStack(s);
+        //1,2,3
+        printStack(s);
+    }
+
 }
