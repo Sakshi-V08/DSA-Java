14f00b9a3ef92152a7e616b7bc6f3849cbed157e
Sakshi-V08 <122119205+Sakshi-V08@users.noreply.github.com>
2023-12-25 14:53:03 +0530

    upload

---
+++ Queue/QueueJCF.java
@@ -0,0 +1,18 @@
+package Queue;
+import java.util.*;
+public class QueueJCF {
+    public static void main(String args[]){
+        //Queue q= new Queue();
+        //Queue<Integer> q = new LinkedList<>();   //ArrayDeque
+        Queue<Integer> q = new ArrayDeque<>();   //O(n)
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+    
+}
