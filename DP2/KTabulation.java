aded8b1378107f5685db976aa6fdd0f95634c460
Sakshi-V08 <122119205+Sakshi-V08@users.noreply.github.com>
2024-03-26 11:21:29 +0530

    commit

--- DP2/KTabulation.java
+++ DP2/KTabulation.java
@@ -24,6 +24,47 @@ public class KTabulation {
          return dp[n][W];
         }
      }
+
+
+     public static void print(int dp[][]){
+        System.out.println("Print whole table of Knapsack:");
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+     }
+     public static int knapsackTab(int val[], int wt[], int W){
+        int n= val.length;
+        int dp[][]= new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=0;
+        }
+
+        for(int i =1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int v= val[i-1]; //ith item val
+                int w= wt[i-1]; //ith item wt
+                if(w <= j){
+                    //valid
+                    int incProfit = v + dp[i-1][j-w];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }else{
+                    //invlaid
+                    int excProfit= dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][W];
+     }
      public static void main(String args[]){
          int val[] ={15,14, 10 , 45,30};
          int wt[] ={2,5,1,3,4};
@@ -35,6 +76,7 @@ public class KTabulation {
                 dp[i][j] = -1;  //initial with -1
             }
          }
-         System.out.println(knapsack(val, wt, W, val.length,dp));
+        //System.out.println(knapsack(val, wt, W, val.length,dp));
+        System.out.print(knapsackTab(val, wt, W));
      }
 }
