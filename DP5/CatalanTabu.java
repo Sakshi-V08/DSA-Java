04badad9f20bccda83382063114a6c41ac4566ab
Sakshi-V08 <122119205+Sakshi-V08@users.noreply.github.com>
2024-11-17 17:04:28 +0530

    commit

--- DP5/CatalanTabu.java
+++ DP5/CatalanTabu.java
@@ -1,5 +1,23 @@
 package DP5;
 
 public class CatalanTabu {
-     
+    // O(n*n)
+    public static int catalanTabo(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) { // Ci
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1]; // Ci = Cj * Ci-j-1
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String args[]) {
+        int n = 4;
+        System.out.println(catalanTabo(4));
+
+    }
 }
